a393e5da774502a3ed8d051163199b53e03a8e7c
Godwin Duliente <rualgodwin@gmail.com>
2024-03-03 15:07:28 +0800

    file restructure

---
+++ server/index.js
@@ -0,0 +1,18 @@
+// Initialize Dependencies
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+const cors = require("cors");
+
+// Run server
+app.listen(3002, () => {
+  console.log("Server is running at port 3002");
+});
+
+// Create DB Connection
+const db = mysql.createConnection({
+  user: "root",
+  host: "localhost",
+  password: "",
+  database: "",
+});
